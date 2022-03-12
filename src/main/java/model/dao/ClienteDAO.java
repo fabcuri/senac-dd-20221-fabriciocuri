a4e51289f8a674bd62c70bd9769e04cd794d3be0
fabcuri <fabcuri70@gmail.com>
2022-03-12 10:39:06 -0300

    Metodos implantados: inserir,atualizar,deletar no telefone dao ecliente dao

--- src/main/java/model/dao/ClienteDAO.java
+++ src/main/java/model/dao/ClienteDAO.java
@@ -1,5 +1,80 @@
 package model.dao;
 
-public class ClienteDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import model.entity.Cliente;
+import model.entity.Endereco;
+
+public class ClienteDAO {
+	public Cliente inserir(Cliente novoCliente) {
+		Connection conexao = Banco.getConnection();
+		String sql = " INSERT INTO CLIENTE(NOME,CPF,ENDERECO,LINHAS)" 
+					+ "VALUES (?, ?, ?, ?);";
+		
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setString(1, novoCliente.getNome());
+			stmt.setString(2, novoCliente.getCpf());
+			stmt.setEndereco(3, novoCliente.getEndereco());
+			stmt.setLinhas(4, novoCliente.getLinhas());
+			
+			stmt.execute();
+			
+			ResultSet chavesGeradas = stmt.getGeneratedKeys();
+			if(chavesGeradas.next()) {
+				novoCliente.setId(chavesGeradas.getInt(1));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao inserir cliente. Causa:" + e.getMessage());
+		}
+		
+		return novoCliente;
 }
+	
+	public boolean atualizar(Cliente cliente) {
+		boolean atualizou = false;
+		Connection conexao = Banco.getConnection();
+		String sql = " UPDATE CLIENTE "
+					+" SET NOME=?, CPF=?, ENDERECO=?, LINHA=? "
+					+" WHERE ID=?";
+		
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setString(1, cliente.getNome());
+			stmt.setString(2, cliente.getCpf());
+			stmt.setEndereco(3, cliente.getEndereco());
+			stmt.setLinhas(4, cliente.getLinhas());
+			stmt.setInt(5, cliente.getId());
+			
+			int linhasAfetadas = stmt.executeUpdate();
+			atualizou = linhasAfetadas > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar cliente. Causa:" + e.getMessage());
+		}
+		
+		return atualizou;
+	}
+	public boolean remover(int id) {
+		boolean removeu = false;
+		
+		Connection conexao = Banco.getConnection();
+		String sql = " DELETE FROM CLIENTE "
+					+" WHERE ID=?";
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setInt(1, id);
+			removeu = stmt.executeUpdate() > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao remover cliente. Causa:" + e.getMessage());
+		}		
+		
+		return removeu;
+	}
+	
+	}
--- src/main/java/model/dao/TelefoneDAO.java
+++ src/main/java/model/dao/TelefoneDAO.java
@@ -1,5 +1,83 @@
 package model.dao;
 
-public class TelefoneDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import model.entity.Endereco;
+import model.entity.Telefone;
+
+public class TelefoneDAO {
+	public Endereco inserir(Telefone novoTelefone) {
+		Connection conexao = Banco.getConnection();
+		String sql = " INSERT INTO TELEFONE(DDD,NUMERO,TIPO,ATIVO)" 
+					+ "VALUES (?, ?, ?, ?);";
+		
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setString(1, novoTelefone.getDdd());
+			stmt.setString(2, novoTelefone.getNumero());
+			stmt.setTipo(3, novoTelefone.getTipo());
+			stmt.setAtivo(4, novoTelefone.isAtivo());
+		
+	
+		
+			
+			stmt.execute();
+			
+			ResultSet chavesGeradas = stmt.getGeneratedKeys();
+			if(chavesGeradas.next()) {
+				novoTelefone.setId(chavesGeradas.getInt(1));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao inserir telefone. Causa:" + e.getMessage());
+		}
+		
+		return novoTelefone;
+	}
+	
+	public boolean atualizar(Telefone telefone) {
+		boolean atualizou = false;
+		Connection conexao = Banco.getConnection();
+		String sql = " UPDATE TELEFONE "
+					+" SET DDD=?, NUMERO=?, TIPO=?, ATIVO=? "
+					+" WHERE ID=?";
+		
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setString(1, telefone.getDdd());
+			stmt.setString(2, telefone.getNumero());
+			stmt.setTipo(3, telefone.getTipo());
+			stmt.setAtivo(4, telefone.isAtivo());
+			stmt.setInt(5, telefone.getId());
+			
+			int linhasAfetadas = stmt.executeUpdate();
+			atualizou = linhasAfetadas > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar telefone. Causa:" + e.getMessage());
+		}
+		
+		return atualizou;
+	}
+	
+	public boolean remover(int id) {
+		boolean removeu = false;
+		
+		Connection conexao = Banco.getConnection();
+		String sql = " DELETE FROM TELEFONE "
+					+" WHERE ID=?";
+		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
+		
+		try {
+			stmt.setInt(1, id);
+			removeu = stmt.executeUpdate() > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao remover telefone. Causa:" + e.getMessage());
+		}		
+		
+		return removeu;
+	}
 }
