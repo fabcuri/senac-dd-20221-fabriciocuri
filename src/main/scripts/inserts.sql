c1e3855d71a305916208058c6281659da8a20e07
fabcuri <fabcuri70@gmail.com>
2022-03-23 12:14:34 -0300

    Arrumando os scripts mysql

--- src/main/scripts/inserts.sql
+++ src/main/scripts/inserts.sql
@@ -1,11 +1,11 @@
-INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Felipe Schimdt', 'FlorianÃ³polis', 'SC', '1', '88005444');
-INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Max Schramm', 'FlorianÃ³polis', 'SC', '3', '88005222');
-INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Presidente Kennedy', 'SÃ£o JosÃ©', 'SC', '2', '88222444');
+INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Felipe Schimdt', 'Florianópolis', 'SC', '1', '88005444');
+INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Max Schramm', 'Florianópolis', 'SC', '3', '88005222');
+INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Presidente Kennedy', 'São José', 'SC', '2', '88222444');
 INSERT INTO ENDERECO (RUA, CIDADE, UF, NUMERO, CEP) VALUES ('Nereu Ramos', 'Lages', 'SC', '105', '88555444');
 
 INSERT INTO telefonia.CLIENTE (id,nome, cpf, id_endereco) VALUES (1,'Edson Arantes','11133322211', 2);
 INSERT INTO telefonia.CLIENTE (id,nome, cpf, id_endereco) VALUES (2,'Artur Antunes','22233322211', 4);
-INSERT INTO telefonia.CLIENTE (id,nome, cpf, id_endereco) VALUES (3,'Marcos AndrÃ©','11133322255', 6);
+INSERT INTO telefonia.CLIENTE (id,nome, cpf, id_endereco) VALUES (3,'Marcos André','11133322255', 3);
 
 INSERT INTO telefonia.TELEFONE (id, ddd, numero, tipo, ativo) VALUES (1,'48','32323232', 0, 1);
 INSERT INTO telefonia.TELEFONE (id, ddd, numero, tipo, ativo) VALUES (2,'48','984213535', 1, 0);
