9f337ecb7eeff21bd405f5f2d3bd17c747184dad
fabcuri <fabcuri70@gmail.com>
2022-03-31 10:39:26 -0300

    Corrigindo problemas na conexão...adaptando para o note pessoal

--- src/main/java/model/dao/Banco.java
+++ src/main/java/model/dao/Banco.java
@@ -15,7 +15,7 @@ public class Banco {
 	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" 
 			+ BANCODADOS + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
 	private static final String USER = "root";
-	private static final String PASSWORD = "admin";
+	private static final String PASSWORD = " ";
 	
 	public static Connection getConnection(){
 		try {
